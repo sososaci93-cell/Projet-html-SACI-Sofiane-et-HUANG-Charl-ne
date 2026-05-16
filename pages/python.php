0b8cdbcf14c030f4f6196d3e04dc539ee78c3824
sososaci93-cell <sososaci93@gmail.com>
2026-05-16 03:40:08 +0200

    Update python.php

--- pages/python.php
+++ pages/python.php
@@ -8,18 +8,23 @@
 </head>
 <body>
     <h1> Bienvenu sur la formation Python !</h1>
+
+    <?php //division generale "conteneur-lecon" ?>
     <div class="conteneur-lecons">
 
+
+        <?php //premiere division "bloc1" ?>
     <div class="bloc1">
         <p class="var">Les variables</p>
         <div class="langages">
-            <a href="variable_cours.php">
+            <a href="variable.php">
                 <img src="https://blog.penjee.com/wp-content/uploads/2020/06/several-examples-of-Python-variables.png" alt="Leçon-Variables">
             </a>
         </div>
-    
-    
     </div> 
+
+
+    <?php //deuxieme division "bloc2" ?>
     <div class="bloc2">
         <p class="liste">Les listes</p> 
         <div class="liste">
@@ -27,7 +32,21 @@
                 <img src="https://user.oc-static.com/upload/2021/07/05/16254683020359_1C5.png" alt="Leçon-Listes">
             </a>
         </div>
-    </div> </div>
+    </div> 
+
+    <?php //troisieme division "bloc3" ?>
+    <div class="bloc3">
+        <p class="boucle">Les boucles</p> 
+        <div class="boucle">
+            <a href="boucle.php">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvtJPnmseeO_2IvhQGJRhPqERnJGqyQ9XSsA&s" alt="Leçon-Boucles">
+            </a>
+        </div>
+    </div>
+
+
+
+</div>
 
 
 
