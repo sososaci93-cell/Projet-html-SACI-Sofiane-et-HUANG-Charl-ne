2d43d5f14603567d0bcc24851a9530976f095175
sososaci93-cell <sososaci93@gmail.com>
2026-05-16 02:31:03 +0200

    Update python.php

--- pages/python.php
+++ pages/python.php
@@ -8,20 +8,26 @@
 </head>
 <body>
     <h1> Bienvenu sur la formation Python !</h1>
-    <div class=conteneur-lecons>
-        <p class="var"> Les variables</p>
-        <div class="langages">
-                <a href="variable.php">
-                    <img src="https://blog.penjee.com/wp-content/uploads/2020/06/several-examples-of-Python-variables.png"  alt="Leçon-Variables">
-                </a>
-            </div>
+    <div class="conteneur-lecons">
 
-        <div class="liste">
-                <a href="liste.php">
-                    <img src="https://user.oc-static.com/upload/2021/07/05/16254683020359_1C5.png"  alt="Leçon-Listes">
-                </a>
-            </div>
+    <div class="bloc1">
+        <p class="var">Les variables</p>
+        <div class="langages">
+            <a href="variable_cours.php">
+                <img src="https://blog.penjee.com/wp-content/uploads/2020/06/several-examples-of-Python-variables.png" alt="Leçon-Variables">
+            </a>
         </div>
+    
+    
+    </div> 
+    <div class="bloc2">
+        <p class="liste">Les listes</p> 
+        <div class="liste">
+            <a href="liste.php">
+                <img src="https://user.oc-static.com/upload/2021/07/05/16254683020359_1C5.png" alt="Leçon-Listes">
+            </a>
+        </div>
+    </div> </div>
 
 
 
